27dfb0e6b5288eada60196ad0af463f6186c5177
exusia1 <87603817+exusia1@users.noreply.github.com>
2021-11-23 19:11:22 +0300

    Update Lab3.py

--- SecondHalfLabs/Lab3.py
+++ SecondHalfLabs/Lab3.py
@@ -7,6 +7,9 @@ from watchdog.events import FileSystemEventHandler
 class Handler(FileSystemEventHandler):
     def on_created(self, event):
         print(event)
+    
+    def on_modified(self, event):
+        print(event)
 
     def on_deleted(self, event):
         print(event)
